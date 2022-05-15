e10bd7f8683e655197bf70f5db4f7b2e616df11b
지금이야 <cz2123xc@gmail.com>
2022-05-16 02:53:55 +0900

    JPA DB 연동 확인중 테스트 코드 작성중

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+}
