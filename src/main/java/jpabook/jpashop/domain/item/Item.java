ceb721d78c701284b5043e6212a56d98cc1ceaf1
지금이야 <cz2123xc@gmail.com>
2022-05-22 01:43:15 +0900

    상품 레포지토리

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,5 +28,30 @@ public abstract class Item {
     @ManyToMany
     @JoinTable(name = "category_item")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+
+    /**
+     * 재고를 증가시키는 메서드
+     * @param quantity
+     */
+    public void addStork(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * 재고를 감소시키는 메서드
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
+
+
+
 
 }
